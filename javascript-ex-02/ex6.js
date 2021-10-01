bff0565467c725abcd832dd60ef44be80d2c4721
Terminator-True <jf28renault@gmail.com>
2021-10-01 11:39:45 +0200

    016

--- javascript-ex-02/ex6.js
+++ javascript-ex-02/ex6.js
@@ -1,3 +1,11 @@
 //Fes una funció en javascript que donada una quantitat d’euros ens retorni el seu valor en tres tipus de monedes. 
 //Utilitza les funcions de callBack, (és a dir funcions com expressions)
 
+function divisa() {
+    let euros = prompt("Quantitat en Euros ")
+    function Mostra(euros,euroDolar,euroYen,euroBit) {
+        document.write(euros+" euros"+" | "+euroYen(euros)+" Yen" +" | "+euroBit(euros)+" Bitcoin"+" | "+euroDolar(euros)+" Dolars")
+    }
+
+    Mostra(euros,EuroDolar = euros => euros*1.16,EuroYen = euros =>euros*128.77134,EuroBit = euros => euros*0.000026)
+}
---
+++ javascript-ex-02/ex7.js
@@ -0,0 +1,2 @@
+//Fes una funció en javascript que retorni el nombre de vegades que un determinat caràcter apareix en un string.
+
