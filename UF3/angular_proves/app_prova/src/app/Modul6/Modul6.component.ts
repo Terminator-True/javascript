9ac152ce1b671cf0c5da22c6d159999c40ead14d
Terminator-True <jf28renault@gmail.com>
2022-01-20 17:35:06 +0100

    059

--- UF3/angular_proves/app_prova/src/app/Modul6/Modul6.component.ts
+++ UF3/angular_proves/app_prova/src/app/Modul6/Modul6.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'modul6',
+    selector: '[Modul6]',
     template:`
-    <h2>Proves modul6</h2>
+    <h2>modul6</h2>
     `
 })
 export class Modul6{
--- UF3/angular_proves/app_prova/src/app/app.module.ts
+++ UF3/angular_proves/app_prova/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Modul6 } from './modul6/modul6.component';
+import { Modul6 } from './Modul6/Modul6.component';
 
 
 @NgModule({
